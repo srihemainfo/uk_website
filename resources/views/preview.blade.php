0d0dfd2e2f650e9f58bae517fa91a62b06c43180
elavarasan-developer <elavarasan.bca23@gmail.com>
2026-08-21 19:01:23 +0530

    clarity added

--- resources/views/preview.blade.php
+++ resources/views/preview.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('partials.seo')
     <title>GoRide | Booking Confirmation Preview</title>
     <link rel="shortcut icon" href="https://www.goride.net.in/goride/img/Go-Ride-fav-icon.webp" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
